e6afb0876990eefb3c347a540054dfca6e59ca1c
Jose M <jmmejias11@gmail.com>
2025-09-16 10:27:29 -0300

    feat: postgres config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,14 +4,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PayrollModule } from './payroll/payroll.module';
 import { EmployeesModule } from './employees/employees.module';
+import { Company } from './companies/companies.entity';
+import { Employee } from './employees/employees.entity';
+import { CompaniesModule } from './companies/companies.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      // your database config
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT as string) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'mysecretpassword',
+      database: process.env.DB_NAME || 'payroll_db',
+      entities: [Company, Employee],
+      synchronize: true,
     }),
     PayrollModule,
     EmployeesModule,
+    CompaniesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
